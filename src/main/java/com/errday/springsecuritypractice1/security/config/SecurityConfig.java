e2498234f4085101dfa01d7ba077b88996ed0efa
javython999 <infinity2544@gmail.com>
2024-11-04 20:21:08 +0900

    Custom AuthenticationFailureHandler, AccessDeniedHandler

--- src/main/java/com/errday/springsecuritypractice1/security/config/SecurityConfig.java
+++ src/main/java/com/errday/springsecuritypractice1/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.errday.springsecuritypractice1.security.config;
 
+import com.errday.springsecuritypractice1.security.handler.FormAccessDeniedHandler;
+import com.errday.springsecuritypractice1.security.handler.FormAuthenticationFailureHandler;
 import com.errday.springsecuritypractice1.security.handler.FormAuthenticationSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -20,21 +22,28 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
     private final FormAuthenticationSuccessHandler formAuthenticationSuccessHandler;
+    private final FormAuthenticationFailureHandler formAuthenticationFailureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                 .requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll() // 정적 자원 설정
-                .requestMatchers("/", "/signup").permitAll()
+                .requestMatchers("/", "/signup", "/login*").permitAll()
+                .requestMatchers("/user").hasAuthority("ROLE_USER")
+                .requestMatchers("/manager").hasAuthority("ROLE_MANAGER")
+                .requestMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated())
             .formLogin(formLogin -> formLogin
                 .loginPage("/login")
                 .authenticationDetailsSource(authenticationDetailsSource) // 추가적인 정보를 인증 객체에 추가할 수 있다. 그 작업을 처리해주는 클래스 지정
-                .successHandler(formAuthenticationSuccessHandler)   // 로그인 성공시 handler 추가
+                .successHandler(formAuthenticationSuccessHandler)   // 로그인 성공 handler 추가
+                .failureHandler(formAuthenticationFailureHandler)   // 로그인 실패 handler 추가
                 .permitAll()) // 커스텀 로그인 페이지 설정
             //.userDetailsService(userDetailsService) // 커스텀 UserDetailServce 설정 (custom authenticationProvider를 설정하면 필요없어진다)
             .authenticationProvider(authenticationProvider) // 커스텀 AuthenticationProvider 설정
+            .exceptionHandling(exceptioin -> exceptioin
+                .accessDeniedHandler(new FormAccessDeniedHandler("/denied")))   // 커스텀 AccessDeniedHandler
         ;
 
         return http.build();
--- src/main/java/com/errday/springsecuritypractice1/users/controller/LoginController.java
+++ src/main/java/com/errday/springsecuritypractice1/users/controller/LoginController.java
@@ -1,18 +1,27 @@
 package com.errday.springsecuritypractice1.users.controller;
 
+import com.errday.springsecuritypractice1.users.domain.dto.AccountDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "exception" , required = false) String exception,
+                        Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+
         return "login/login";
     }
 
@@ -30,6 +39,15 @@ public class LoginController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/denied")
+    public String denied(@RequestParam(name = "exception", required = false) String exception,
+                         @AuthenticationPrincipal AccountDto authentication,
+                         Model model) {
+        model.addAttribute("exception", exception);
+        model.addAttribute("username", authentication.getUsername());
+        return "login/denied";
     }
 
 }
